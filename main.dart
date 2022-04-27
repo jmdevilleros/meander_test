b6ae75cff04215fe58af0a3217c7f9a493bae61a
Juan de Villeros <41244769+Shaular@users.noreply.github.com>
2022-04-26 19:11:01 -0500

    Update main.dart
    Added curve animation

--- main.dart
+++ main.dart
@@ -3,14 +3,17 @@
 // main.dart - App entry point
 // =============================================================================
 
+import 'dart:async';
 import 'dart:math';
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
+
+final _isRunningNotifier = ValueNotifier<bool>(false);
+const _runningDuration = Duration(seconds: 60);
 
 // -----------------------------------------------------------------------------
 void main() {
-    runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 // -----------------------------------------------------------------------------
@@ -60,7 +63,7 @@ class MainPage extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        const RemainingTime(),
+        const HeaderField(),
         buildPlayZone(),
         const StartButton(),
       ],
@@ -80,22 +83,79 @@ class MainPage extends StatelessWidget {
 }
 
 // -----------------------------------------------------------------------------
-class RemainingTime extends StatelessWidget {
-  const RemainingTime({
+class HeaderField extends StatelessWidget {
+  const HeaderField({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    /// todo: show only after start
-    /// todo: if started, timer else "press run button"
-    return const Padding(
-      padding: EdgeInsets.only(bottom: 20.0),
-      child: Text(
-        "30 seconds remaining",
-        style: TextStyle(fontSize: 20),
-      ),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: ValueListenableBuilder(
+          valueListenable: _isRunningNotifier,
+          builder: (_, bool isRunning, __) {
+            if (isRunning) {
+              return const CountDownTimer(lapse: _runningDuration);
+            } else {
+              return const SizedBox(height: 15);
+            }
+          }),
     );
+  }
+}
+
+// -----------------------------------------------------------------------------
+class CountDownTimer extends StatefulWidget {
+  final Duration lapse;
+
+  const CountDownTimer({
+    Key? key,
+    required this.lapse,
+  }) : super(key: key);
+
+  // ---------------------------------------------------------------------------
+  @override
+  State<CountDownTimer> createState() => _CountDownTimerState();
+}
+
+class _CountDownTimerState extends State<CountDownTimer> {
+  late Timer timer;
+  late Duration remain;
+
+  // ---------------------------------------------------------------------------
+  void onTick(Timer timer) {
+    remain = Duration(seconds: widget.lapse.inSeconds - timer.tick);
+    if (remain <= Duration.zero || _isRunningNotifier.value == false) {
+      _isRunningNotifier.value = false;
+      timer.cancel();
+    }
+    setState(() {});
+  }
+
+  // ---------------------------------------------------------------------------
+  @override
+  initState() {
+    super.initState();
+    timer = Timer.periodic(const Duration(seconds: 1), onTick);
+    remain = Duration(seconds: widget.lapse.inSeconds - timer.tick);
+  }
+
+  // ---------------------------------------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+    final String remainRepr = remain.toString();
+    return Text(
+      remainRepr.substring(
+          remainRepr.indexOf(":") + 1, remainRepr.lastIndexOf(".")),
+    );
+  }
+
+  // ---------------------------------------------------------------------------
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
   }
 }
 
@@ -107,18 +167,25 @@ class StartButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
+    return FloatingActionButton(
+      foregroundColor: Theme.of(context).scaffoldBackgroundColor,
       onPressed: () async {
-        /// todo: do nothing if running
-        await doCountDown(context);
-        /// todo: start game
+        if (_isRunningNotifier.value == true) {
+          _isRunningNotifier.value = false;
+        } else {
+          await doCountDown(context);
+          _isRunningNotifier.value = true;
+        }
       },
-      child: Icon(
-        Icons.directions_run,
-
-        /// todo: change color if running (disable?)
-        color: Theme.of(context).textTheme.bodyText2?.color,
-        size: 50,
+      child: ValueListenableBuilder(
+        valueListenable: _isRunningNotifier,
+        child: const Icon(Icons.directions_run),
+        builder: (_, bool isRunning, __) {
+          return Icon(
+            isRunning == true ? Icons.stop : Icons.play_arrow,
+            size: 40,
+          );
+        },
       ),
     );
   }
@@ -131,6 +198,7 @@ class StartButton extends StatelessWidget {
       await Future.delayed(const Duration(seconds: 1), () => secondsToStart--);
     } while (secondsToStart > 0);
     showCountdown(context, "GO");
+    await Future.delayed(const Duration(milliseconds: 500));
   }
 
   // ---------------------------------------------------------------------------
@@ -155,7 +223,6 @@ class StartButton extends StatelessWidget {
 }
 
 // -----------------------------------------------------------------------------
-/// todo: este es el que se convioerte a statful y controla las demas
 class MeanderZone extends StatelessWidget {
   const MeanderZone({
     Key? key,
@@ -163,10 +230,12 @@ class MeanderZone extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width / 1.618 / 2;
+    final height = MediaQuery.of(context).size.height / 1.618 / 1.618;
     return Column(
       children: [
         collisionTimer(context),
-        const MeanderBox(),
+        MeanderBox(size: Size(width, height)),
         controlButtons(context),
       ],
     );
@@ -176,8 +245,8 @@ class MeanderZone extends StatelessWidget {
   Widget controlButtons(BuildContext context) {
     return Row(
       children: const [
-        ControlButton(iconData: Icons.arrow_left),
-        ControlButton(iconData: Icons.arrow_right),
+        ControlButton(iconData: Icons.arrow_back),
+        ControlButton(iconData: Icons.arrow_forward),
       ],
     );
   }
@@ -206,6 +275,7 @@ class ControlButton extends StatelessWidget {
       margin: const EdgeInsets.only(
         left: 10,
         right: 10,
+        bottom: 20,
       ),
       decoration: BoxDecoration(
         shape: BoxShape.circle,
@@ -213,9 +283,7 @@ class ControlButton extends StatelessWidget {
       ),
       child: IconButton(
         iconSize: 40,
-        icon: Icon(
-          iconData,
-        ),
+        icon: Icon(iconData),
         onPressed: () {},
       ),
     );
@@ -223,24 +291,66 @@ class ControlButton extends StatelessWidget {
 }
 
 // -----------------------------------------------------------------------------
-class MeanderBox extends StatelessWidget {
+class MeanderBox extends StatefulWidget {
+  final Size size;
+
   const MeanderBox({
     Key? key,
+    required this.size,
   }) : super(key: key);
 
   @override
+  State<MeanderBox> createState() => _MeanderBoxState();
+}
+
+class _MeanderBoxState extends State<MeanderBox>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController controller;
+  late final Animation<double> animation;
+  double plotStartY = 0;
+
+  // ---------------------------------------------------------------------------
+  double plotFunction(double y) {
+    return 10 * sin(2 * pi / 100 * y + plotStartY) + 0 + 30;
+  }
+
+  // ---------------------------------------------------------------------------
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(duration: _runningDuration, vsync: this);
+    animation =
+        Tween<double>(begin: 0, end: widget.size.height).animate(controller);
+    animation.addListener(() => moveCurve());
+    _isRunningNotifier.addListener(() => checkIfRunning());
+  }
+
+  // ---------------------------------------------------------------------------
+  void checkIfRunning() {
+    if (_isRunningNotifier.value == true) {
+      controller.reset();
+      setState(() {});
+      controller.forward();
+    } else {
+      controller.stop();
+    }
+  }
+
+  // ---------------------------------------------------------------------------
+  @override
+  void dispose() {
+    animation.removeListener(() => moveCurve());
+    controller.dispose();
+    _isRunningNotifier.removeListener(() => checkIfRunning());
+    super.dispose();
+  }
+
+  // ---------------------------------------------------------------------------
+  @override
   Widget build(BuildContext context) {
-    final double width = MediaQuery.of(context).size.width / 1.618 / 2;
-    final double height = MediaQuery.of(context).size.height / 1.618 / 1.618;
-
-    /// todo: receive function as parameter? or choose from a global list?
-    /// todo: must remain fixed after selecting it for each meander
-    double meanderPlotFunction(double y) =>
-        10 * sin(2 * pi / 100 * y + 0) + 0 + 30;
-
     return Container(
-      width: width,
-      height: height,
+      width: widget.size.width,
+      height: widget.size.height,
       margin: const EdgeInsets.all(20.0),
       decoration: BoxDecoration(
         borderRadius: const BorderRadius.all(
@@ -251,13 +361,18 @@ class MeanderBox extends StatelessWidget {
       child: Stack(
         children: [
           CustomPaint(
-            size: Size(width, height),
-            painter: MeanderPainter(plotFunction: meanderPlotFunction),
+            size: widget.size,
+            painter: MeanderPainter(plotFunction: plotFunction),
           ),
-          const MeanderToken(),
+          const MeanderToken(size: Size(30, 30), offset: Offset(50, 120))
         ],
       ),
     );
+  }
+
+  // ---------------------------------------------------------------------------
+  void moveCurve() {
+    setState(() => plotStartY = animation.value);
   }
 }
 
@@ -292,24 +407,29 @@ class MeanderPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
+    return _isRunningNotifier.value == true;
   }
 }
 
 // -----------------------------------------------------------------------------
 class MeanderToken extends StatelessWidget {
+  final Size size;
+  final Offset offset;
+
   const MeanderToken({
     Key? key,
+    required this.size,
+    required this.offset,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     /// todo: left, right as parameters
     return Positioned(
-      width: 30,
-      height: 30,
-      left: 50,
-      top: 120,
+      width: size.width,
+      height: size.height,
+      left: offset.dx,
+      top: offset.dy,
       child: Container(
         decoration: const BoxDecoration(
           shape: BoxShape.circle,
