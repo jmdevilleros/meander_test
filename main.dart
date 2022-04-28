8c500c1f094529c23d623c4bd6ab3372d0a11a36
Juan de Villeros <41244769+Shaular@users.noreply.github.com>
2022-04-28 00:13:23 -0500

    Update main.dart
    Finish alpha version

--- main.dart
+++ main.dart
@@ -99,10 +99,10 @@ class HeaderField extends StatelessWidget {
           valueListenable: _isRunningNotifier,
           builder: (_, bool isRunning, __) {
             return DefaultTextStyle(
-                style: const TextStyle(fontSize: 15),
+                style: const TextStyle(fontSize: 15, color: Colors.white),
                 child: isRunning
                     ? const CountDownTimer(lapse: _runningDuration)
-                    : const Text(""));
+                    : const Text("00:00"));
           }),
     );
   }
@@ -234,12 +234,22 @@ class MeanderZone extends StatelessWidget {
     final movement = TokenMovement()..direction = TokenDirection.neutral;
     final width = MediaQuery.of(context).size.width / 1.618 / 1.618;
     final height = MediaQuery.of(context).size.height / 1.618 / 1.618;
+    final collisionNotifier = ValueNotifier<int>(0);
+
     return Column(
       children: [
-        collisionTimer(context),
+        ValueListenableBuilder(
+            valueListenable: collisionNotifier,
+            builder: (_, int millis, __) {
+              return Text(
+                "$millis",
+                style: Theme.of(context).textTheme.bodyText2,
+              );
+            }),
         MeanderBox(
           size: Size(width, height),
           movement: movement,
+          collisionNotifier: collisionNotifier,
         ),
         buildControlButtons(movement),
       ],
@@ -251,24 +261,17 @@ class MeanderZone extends StatelessWidget {
     return Row(
       children: [
         ControlButton(
-          iconData: Icons.arrow_back,
+          iconData: Icons.chevron_left,
           direction: TokenDirection.left,
           movement: movement,
         ),
+        const SizedBox(width: 20),
         ControlButton(
-          iconData: Icons.arrow_forward,
+          iconData: Icons.chevron_right,
           direction: TokenDirection.right,
           movement: movement,
         ),
       ],
-    );
-  }
-
-  // ---------------------------------------------------------------------------
-  Widget collisionTimer(BuildContext context) {
-    return Text(
-      "Collision time",
-      style: Theme.of(context).textTheme.bodyText2,
     );
   }
 }
@@ -297,7 +300,7 @@ class ControlButton extends StatelessWidget {
           shape: BoxShape.circle,
           color: Theme.of(context).textTheme.bodyText2?.color,
         ),
-        child: Icon(iconData, size: 40),
+        child: Icon(iconData, size: 50),
       ),
     );
   }
@@ -307,11 +310,13 @@ class ControlButton extends StatelessWidget {
 class MeanderBox extends StatefulWidget {
   final Size size;
   final TokenMovement movement;
+  final ValueNotifier<int> collisionNotifier;
 
   const MeanderBox({
     Key? key,
     required this.size,
     required this.movement,
+    required this.collisionNotifier,
   }) : super(key: key);
 
   @override
@@ -322,25 +327,23 @@ class _MeanderBoxState extends State<MeanderBox>
     with SingleTickerProviderStateMixin {
   late final AnimationController controller;
   late final Animation<double> animation;
-  late final double meanderWidth;
-  late final double plotShiftX;
-  late final Size tokenSize;
-
   final Offset tokenSpeed = const Offset(1, 0);
+  late double meanderWidth;
+  late double plotAmplitude;
+  late double plotPeriod;
+  late double plotShiftX;
+  late double plotShiftY;
+  late Size tokenSize;
+  Color tokenColor = Colors.red;
   Offset tokenOffset = const Offset(0, 0);
-
   double plotStartY = 0;
-  final plotAmplitude = Random().nextInt(10) + 20;
-  final plotPeriod = Random().nextInt(60) + 140;
-  final plotShiftY = Random().nextInt(150);
+  Timer? collisionTimer;
 
   // ---------------------------------------------------------------------------
   @override
   void initState() {
     super.initState();
-    plotShiftX = widget.size.width / 4;
-    meanderWidth = plotShiftX * 2;
-    tokenSize = Size(meanderWidth / 2.5, meanderWidth / 2.5);
+    initParameters();
     setStartOffset();
     controller = AnimationController(duration: _runningDuration, vsync: this);
     animation =
@@ -350,11 +353,24 @@ class _MeanderBoxState extends State<MeanderBox>
   }
 
   // ---------------------------------------------------------------------------
+  void initParameters() {
+    meanderWidth = 3 * widget.size.width / 7;
+    plotShiftX = 2 * widget.size.width / 7;
+    plotShiftY = Random().nextDouble() * widget.size.height;
+    tokenSize = Size(meanderWidth / 3, meanderWidth / 3);
+    plotAmplitude =
+        widget.size.width / 5 - Random().nextInt(tokenSize.width ~/ 5);
+    plotPeriod = widget.size.height;
+    widget.collisionNotifier.value = 0;
+  }
+
+  // ---------------------------------------------------------------------------
   @override
   void dispose() {
     animation.removeListener(() => onAnimationCycle());
     _isRunningNotifier.removeListener(() => onChangeRunning());
     controller.dispose();
+    collisionTimer?.cancel();
     super.dispose();
   }
 
@@ -380,7 +396,7 @@ class _MeanderBoxState extends State<MeanderBox>
               meanderWidth: meanderWidth,
             ),
           ),
-          MeanderToken(size: tokenSize, offset: tokenOffset)
+          MeanderToken(size: tokenSize, offset: tokenOffset, color: tokenColor)
         ],
       ),
     );
@@ -389,38 +405,37 @@ class _MeanderBoxState extends State<MeanderBox>
   // ---------------------------------------------------------------------------
   double plotFunction(double y) {
     final double B = 2 * pi / plotPeriod;
-    return plotAmplitude * sin(B * (y - plotShiftY) + plotStartY / 3) +
+    const speed = 10;
+    return plotAmplitude * sin(B * (y + plotShiftY + plotStartY * speed)) +
         plotShiftX;
   }
 
   // ---------------------------------------------------------------------------
   void setStartOffset() {
-    final startY = (widget.size.height - tokenSize.height) / 3;
-    tokenOffset = Offset(
-      plotFunction(startY) + (meanderWidth - tokenSize.width) / 3,
-      startY,
-    );
+    final double startY = widget.size.height / 3;
+    final double startX = widget.size.width / 2;
+    tokenOffset =
+        Offset(startX - tokenSize.width / 2, startY - tokenSize.height / 2);
+
+    bool isColliding = true;
+    do {
+      switch (checkCollision()) {
+        case TokenDirection.left:
+          tokenOffset += tokenSpeed * 10;
+          break;
+        case TokenDirection.neutral:
+          isColliding = false;
+          break;
+        case TokenDirection.right:
+          tokenOffset -= tokenSpeed * 10;
+          break;
+      }
+    } while (isColliding);
   }
 
   // ---------------------------------------------------------------------------
   void onAnimationCycle() {
     plotStartY = animation.value;
-
-    final TokenDirection collisionDirection = checkCollision();
-
-    switch (collisionDirection) {
-      case TokenDirection.left: // Collision with left side of meander
-        tokenOffset += tokenSpeed;
-        widget.movement.direction = TokenDirection.neutral;
-        break;
-      case TokenDirection.neutral: // No collision
-        break;
-      case TokenDirection.right: // Collision with right side of meander
-        tokenOffset -= tokenSpeed;
-        widget.movement.direction = TokenDirection.neutral;
-        break;
-    }
-
     switch (widget.movement.direction) {
       case TokenDirection.left:
         tokenOffset -= tokenSpeed;
@@ -431,7 +446,31 @@ class _MeanderBoxState extends State<MeanderBox>
         tokenOffset += tokenSpeed;
         break;
     }
-
+    bool isColliding = true;
+    switch (checkCollision()) {
+      case TokenDirection.left: // Collision with left side of meander
+        tokenOffset += tokenSpeed * 2;
+        break;
+      case TokenDirection.neutral: // No collision
+        isColliding = false;
+        break;
+      case TokenDirection.right: // Collision with right side of meander
+        tokenOffset -= tokenSpeed * 2;
+        break;
+    }
+    if (isColliding) {
+      tokenColor = Colors.deepOrangeAccent;
+      if (collisionTimer == null || collisionTimer?.isActive == false) {
+        collisionTimer = Timer.periodic(const Duration(milliseconds: 1), (_) {
+          widget.collisionNotifier.value++;
+        });
+      }
+    } else {
+      tokenColor = Colors.red;
+      if (collisionTimer?.isActive == true) {
+        collisionTimer?.cancel();
+      }
+    }
     setState(() {});
   }
 
@@ -440,10 +479,12 @@ class _MeanderBoxState extends State<MeanderBox>
     if (_isRunningNotifier.value == true) {
       controller.reset();
       setStartOffset();
+      initParameters();
       setState(() {});
       controller.forward();
     } else {
       controller.stop();
+      collisionTimer?.cancel();
     }
   }
 
@@ -456,20 +497,20 @@ class _MeanderBoxState extends State<MeanderBox>
     final x2 = plotFunction(y2);
     final x3 = x1 + meanderWidth;
     final x4 = x2 + meanderWidth;
+    final x5 = plotFunction(tokenOffset.dy + tokenSize.height / 2);
+    final x6 = x5 + meanderWidth;
 
     // Check collision with left side of meander
     if (tokenOffset.dx + tolerance <= x1 ||
         tokenOffset.dx + tolerance <= x2 ||
-        tokenOffset.dx <= plotFunction(tokenOffset.dy + tokenSize.height / 2)) {
+        tokenOffset.dx <= x5) {
       return TokenDirection.left;
     }
 
     // check collision with right side of meander
     if (tokenOffset.dx + tokenSize.width - tolerance >= x3 ||
         tokenOffset.dx + tokenSize.width - tolerance >= x4 ||
-        tokenOffset.dx + tokenSize.width >=
-            plotFunction(tokenOffset.dy + tokenSize.height / 2) +
-                meanderWidth) {
+        tokenOffset.dx + tokenSize.width >= x6) {
       return TokenDirection.right;
     }
     // No collision
@@ -491,11 +532,12 @@ class MeanderPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     final plotPaint = Paint()
       ..color = Colors.yellow
-      ..strokeWidth = 1
+      ..strokeWidth = 2
       ..style = PaintingStyle.stroke;
 
     final leftSidePoints = <Offset>[
-      for (double y = 1; y < size.height; y += 1) Offset(plotFunction(y), y)
+      for (double y = 1; y <= size.height - 5; y += 1)
+        Offset(plotFunction(y), y)
     ];
 
     final rightSidePoints = <Offset>[
@@ -516,11 +558,13 @@ class MeanderPainter extends CustomPainter {
 class MeanderToken extends StatelessWidget {
   final Size size;
   final Offset offset;
+  final Color color;
 
   const MeanderToken({
     Key? key,
     required this.size,
     required this.offset,
+    required this.color,
   }) : super(key: key);
 
   @override
@@ -531,10 +575,7 @@ class MeanderToken extends StatelessWidget {
       left: offset.dx,
       top: offset.dy,
       child: Container(
-        decoration: const BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.red,
-        ),
+        decoration: BoxDecoration(shape: BoxShape.circle, color: color),
       ),
     );
   }
